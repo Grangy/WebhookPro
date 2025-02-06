17ea70795289f796bf18858d43c931fab8eb7155
Grangy <m.dolia2017@yandex.ru>
2025-02-06 11:20:14 +0300

    UpdageFront

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-// app.js
 const express = require('express');
+const path = require('path');
+const cors = require('cors');
 const connectDB = require('./config/db');
 const dotenv = require('dotenv');
 const leadRoutes = require('./routes/leadRoutes');
-const path = require('path');
-const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
 
+// CORS: Разрешаем все домены (если фронтенд и API на одном домене, можно не использовать CORS)
 app.use(cors({
-  origin: '*', // Разрешить запросы с любых источников
-  methods: ['GET', 'POST'], // Разрешенные HTTP методы
-  allowedHeaders: ['Content-Type', 'Authorization'], // Разрешенные заголовки
+  origin: '*',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 // Подключаем базу данных
@@ -22,14 +22,18 @@ connectDB();
 // Мидлвар для парсинга JSON тела запросов
 app.use(express.json());
 
-// Маршруты
-app.use('/api', leadRoutes);
+// Раздача статики для фронтенда
+app.use(express.static(path.join(__dirname, 'views')));
 
-// Маршрут для отображения веб-морды
+// Маршрут для фронтенда
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
+// Маршрут для API
+app.use('/api', leadRoutes);
+
+// Запуск сервера
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
